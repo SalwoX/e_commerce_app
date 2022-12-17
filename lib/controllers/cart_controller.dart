398604795262c63e8bc6b39938b61c84e4549516
Ömer Tüfekci <78084742+SalwoX@users.noreply.github.com>
2022-12-17 17:38:22 +0300

    local

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -24,7 +24,7 @@ class CartController extends GetxController {
           img: value.img,
           quantity: value.quantity! + quantity,
           isExist: true,
-          time: "25 Kasım 1989",
+          time: "25 Ağustos 1989",
         );
       });
     } else {
